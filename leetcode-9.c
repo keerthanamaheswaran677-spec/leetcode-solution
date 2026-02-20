dcacc683df5f951baecbcc6542e6274ec9d5b81c
Keerthana M <keerthanamaheswaran677@gmail.com>
2026-02-20 09:53:26 +0530

    Create leetcode-9.c

---
+++ leetcode-9.c
@@ -0,0 +1,16 @@
+bool isPalindrome(int x) {
+    if(x<0) 
+     return 0;
+    int original=x;
+    int rev=0;
+    int digit;
+    while(x>0){
+        digit=x%10;
+        rev=rev*10+digit;
+        x=x/10;
+    }
+   if(original==rev)
+        return true;
+    else
+        return false;
+}
